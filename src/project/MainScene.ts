1f68b5ba1dad4ccb7907de44ba18e5d093597186
Alex Batistta <batistta.alex@outlook.com>
2024-01-08 16:35:33 -0300

    CNAME

--- src/project/MainScene.ts
+++ src/project/MainScene.ts
@@ -3,5 +3,6 @@ import { PixiScene } from "../engine/scenemanager/scenes/PixiScene";
 export class MainScene extends PixiScene {
 	constructor() {
 		super();
+		window.location.href = "https://www.savedate.com.ar/alex-mica";
 	}
 }
